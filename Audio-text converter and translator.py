65e54c1930cb5b553cfbd886b7b3e4fb1b21bfba
Pratiksha Sahoo <sahoopratiksha24@gmail.com>
2023-10-08 19:47:31 +0530

    Add files via upload

---
+++ Audio-text converter and translator.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[58]:
+
+
+#Speech-text
+import speech_recognition as sr
+from deep_translator import GoogleTranslator
+r = sr.Recognizer()
+mic = sr.Microphone(device_index=1)
+with mic as source:
+        print("Listening...")
+        r.adjust_for_ambient_noise(source)
+        audio = r.listen(source)
+        print("Recognizing...")
+        #Recognize speech using the Google Web Speech API
+        text = r.recognize_google(audio)
+        print("Recognized text (default language):")
+        print(text)
+        # Translate speech in your desired language
+        text_lang = GoogleTranslator(source='auto', target='en').translate(text)
+        print(f"Recognized text (translated language):")
+        print(text_lang)
+
+
+# In[56]:
+
+
+#Text-speech
+from win32com.client import constants, Dispatch
+Msg = "Ola, como estas!"
+speaker = Dispatch("SAPI.SpVoice")
+speaker.Speak(Msg)
+del speaker
+
